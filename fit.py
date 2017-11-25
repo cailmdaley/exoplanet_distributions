267b2ad6141e7583d6c96b58130c5985796b23ee
Cail Daley <cdaley@Cails-MacBook-Pro.local>
2017-11-25 18:43:17 -0500

    un-ignored some figures, set a crude sort of parallelization in fit.py

--- fit.py
+++ fit.py
@@ -1,4 +1,17 @@
-dist = Distribution('planets3/')
-ms = np.round(np.logspace(np.log10(0.1), np.log10(13*u.Mjup.to('Mearth')),100),3)[::-1]
-aas = np.round(np.logspace(np.log10(0.05), np.log10(10),100), 3)[::-1]
-dist.fit(ms=ms, aas=aas)
+import numpy as np
+import classes
+
+dist = classes.Distribution('planets3_bottomup/')
+ms = np.round(np.logspace(np.log10(0.1), np.log10(13*u.Mjup.to('Mearth')),100),3)
+aas = np.round(np.logspace(np.log10(0.05), np.log10(10),100), 3)
+
+dist.fit(ms=ms, aas=aas[0:5])
+# dist.fit(ms=ms, aas=aas[5:10])
+# dist.fit(ms=ms, aas=aas[10:15])
+# dist.fit(ms=ms, aas=aas[15:20])
+# dist.fit(ms=ms, aas=aas[20:25])
+# dist.fit(ms=ms, aas=aas[25:30])
+# dist.fit(ms=ms, aas=aas[30:35])
+# dist.fit(ms=ms, aas=aas[35:40])
+# dist.fit(ms=ms, aas=aas[40:45])
+# dist.fit(ms=ms, aas=aas[45:50])
