93bd77ab271def10c04d9b59cb45368b5e80e24a
cailmdaley <cailmdaley@gmail.com>
2017-11-27 20:00:59 -0500

    updated table

--- analyze.py
+++ analyze.py
@@ -1,7 +1,7 @@
 import classes
 
 dist = classes.Distribution(['planets3_bottomup', '../exo_project/planets3'])
-dist.plot('intrinsic', save='figures/planets3_intrinsic')
-dist.plot('observed', save='figures/planets3_observed')
-dist.compare(save='figures/planets3_comparison')
-dist.correlations(save='figures/planets3_correlations')
+#dist.plot('intrinsic', save='figures/planets3_intrinsic')
+#dist.plot('observed', save='figures/planets3_observed')
+#dist.compare(save='figures/planets3_comparison')
+#dist.correlations(save='figures/planets3_correlations')
